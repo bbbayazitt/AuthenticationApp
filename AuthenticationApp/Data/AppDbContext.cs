898cffdda4dde81f158c8d21afbe6d5bfea8c02f
bbbayazitt <berkay01907@gmail.com>
2024-08-01 21:43:03 -0400

    AppDbContext class created to communicate with database

---
+++ AuthenticationApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity; //import the Microsoft.AspNetCore.Identity namespace, which provides the core user identity classes for application.
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace AuthenticationApp.Data // declares namespace for the code.
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser> //automatically includes all the database tables and relationship needed for user authentication.
+    {
+        public AppDbContext(DbContextOptions<DbContext> options) :base(options) 
+        { 
+        
+        }
+    }
+}
