50889c2253263c264b33776c8b1728fd875f7112
bbbayazitt <berkay01907@gmail.com>
2024-08-01 22:55:54 -0400

    accountcontroller controller created and login,logout,register functions are added

---
+++ AuthenticationApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
