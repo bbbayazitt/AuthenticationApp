61c5d0979624d7d95fd8473bb4617a563072ef0a
bbbayazitt <berkay01907@gmail.com>
2024-08-01 22:50:38 -0400

    RegisterVM configurated

--- AuthenticationApp/ViewModel/RegisterVM.cs
+++ AuthenticationApp/ViewModel/RegisterVM.cs
@@ -1,6 +1,25 @@
-﻿namespace AuthenticationApp.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApp.ViewModel
 {
     public class RegisterVM
     {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password doesnt match")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+
+        public string? Adress { get; set; }
     }
 }
