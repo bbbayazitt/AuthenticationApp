2175584e4ba203cddea300b895b0cacc3eb991d2
bbbayazitt <berkay01907@gmail.com>
2024-08-02 00:14:28 -0400

    http post methods added for login and register

--- AuthenticationApp/Controllers/AccountController.cs
+++ AuthenticationApp/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthenticationApp.Models;
+using AuthenticationApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,14 +25,57 @@ namespace AuthenticationApp.Controllers
             return View();
         }
 
-        public IActionResult Logout()
+        [HttpPost]
+
+        public async Task<IActionResult> LoginAsync(LoginVM model)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName!, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Login Attempt");
+                return View(model);
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> RegisterAsync(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new()
+                {
+                    thisName = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Adress = model.Address
+                };
+                var result = await _userManager.CreateAsync(appUser, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(appUser, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Login Attempt");
+                return View(model);
+            }
+            return View(model);
         }
 
 
