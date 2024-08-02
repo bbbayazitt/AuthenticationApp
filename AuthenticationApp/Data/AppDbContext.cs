542357e64a2d50602226e9e20a6b7219502fe26f
bbbayazitt <berkay01907@gmail.com>
2024-08-01 21:50:33 -0400

    AppUser model class created

--- AuthenticationApp/Data/AppDbContext.cs
+++ AuthenticationApp/Data/AppDbContext.cs
@@ -2,9 +2,10 @@
 using Microsoft.AspNetCore.Identity; //import the Microsoft.AspNetCore.Identity namespace, which provides the core user identity classes for application.
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AuthenticationApp.Models;
 namespace AuthenticationApp.Data // declares namespace for the code.
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser> //automatically includes all the database tables and relationship needed for user authentication.
+    public class AppDbContext : IdentityDbContext<AppUser> //automatically includes all the database tables and relationship needed for user authentication.
     {
         public AppDbContext(DbContextOptions<DbContext> options) :base(options) 
         { 
---
+++ AuthenticationApp/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApp.Models
+{
+    public class AppUser : IdentityUser //instead of using <identiyyuser> create our class and use this class features. 
+    {
+        [StringLength(100)]
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email {  get; set; }
+
+    }
+}
